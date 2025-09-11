07a2bd8059ed396cbc7b32b3f004abb8e4e7546b
nguyenthinga-itpro <nguyenthinga.itpro@gmail.com>
2025-09-12 01:11:34 +0700

    push code for login

---
+++ src/Constant/Videos.jsx
@@ -0,0 +1,9 @@
+import RobotLogin from "../assets/Videos/Login.mp4";
+import RobotRegister from "../assets/Videos/Register.mp4";
+
+const Videos = {
+  RobotLogin,
+  RobotRegister,
+};
+
+export default Videos;
